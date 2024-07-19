bb0e37b3c7a546e3285ed8a01e7186ea863f6c1f
정종화 <48465528+JJH-Log@users.noreply.github.com>
2024-07-19 16:05:35 +0900

    Fix: Update SsamApplication.java

--- BACK/Ssam/src/main/java/com/example/demo/SsamApplication.java
+++ BACK/Ssam/src/main/java/com/example/demo/SsamApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SsamApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(SsamApplication.class, args);
 	}
 
