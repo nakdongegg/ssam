c08d9898ffdc092a5a9bcefbce42dc24d31dc9bf
kdhn97 <ehdgus3726@naver.com>
2024-07-21 23:34:42 +0900

    FEAT: 회원정보

--- FRONT/Frontend/src/teacher/myPage/PasswordChange.jsx
+++ FRONT/Frontend/src/teacher/myPage/PasswordChange.jsx
@@ -1,17 +1,43 @@
-import change from "./PasswordChange.module.css"
+import change from "./PasswordChange.module.css";
 
 const PasswordChange = () => {
   return (
     <div>
       <div className={change.menuArray}>
         <div className={change.updateMenu}>
-          <h1>회원정보 수정</h1>
+          <h2>회원정보 수정</h2>
         </div>
         <div className={change.changeMenu}>
-          <h1>비밀번호 변경</h1>
+          <h2>비밀번호 변경</h2>
         </div>
       </div>
-      <h1>비밀번호 변경</h1>
+      <div className={change.formArray}>
+        <div className={change.passwordFormArray}>
+          <h1 className={change.formTxt}>
+            새로 사용할 비밀번호를 입력해주세요
+          </h1>
+          <hr />
+          <div className={change.passwordForm}>
+            <div className={change.inputForm}>
+              <h2>현재 비밀번호</h2>
+              <input type="text" placeholder="Password" className={change.inputTxtForm}></input>
+            </div>
+            <div className={change.inputForm}>
+              <h2>새 비밀번호</h2>
+              <input type="text" placeholder="Password" className={change.inputTxtForm}></input>
+              <p>8~20자의 영문, 숫자, 특수문자를 사용하세요.</p>
+            </div>
+            <div className={change.inputForm}>
+              <h2>새 비밀번호 확인</h2>
+              <input type="text" placeholder="Password" className={change.inputTxtForm}></input>
+            </div>
+            <div className={change.formBtnArray}>
+          <button className={change.formBtn}>저장</button>
+          <button className={change.formBtn}>취소</button>
+        </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- FRONT/Frontend/src/teacher/myPage/TeacherUpdate.jsx
+++ FRONT/Frontend/src/teacher/myPage/TeacherUpdate.jsx
@@ -5,14 +5,55 @@ const TeacherUpdate = () => {
     <div>
       <div className={update.menuArray}>
         <div className={update.updateMenu}>
-          <h1>회원정보 수정</h1>
+          <h2>회원정보 수정</h2>
         </div>
         <div className={update.changeMenu}>
-          <h1>비밀번호 변경</h1>
+          <h2>비밀번호 변경</h2>
         </div>
       </div>
       <div className={update.infoArray}>
-
+        <div className={update.infoForm}>
+          <table className={update.tableArray}>
+            <tr>
+              <th>사진</th>
+              <td>
+                <div className={update.profileImg}></div>
+                <div className={update.btn}>
+                  <button className={update.imgBtn}>수정</button>
+                  <button className={update.imgBtn}>삭제</button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th>이름</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>생년월일</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>학교</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>아이디</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>이메일</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>휴대전화</th>
+              <td></td>
+            </tr>
+          </table>
+        </div>
+        <div className={update.formBtnArray}>
+          <button className={update.formBtn}>저장</button>
+          <button className={update.formBtn}>취소</button>
+        </div>
       </div>
     </div>
   );
